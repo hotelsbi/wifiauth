298eb31d1747985641dcf93725a2c108e1dfbf78
Leon Vaidman <leonvaidman@gmail.com>
2016-09-23 22:07:46 +0300

    set demo.ejs as default page

--- app.js
+++ app.js
@@ -31,17 +31,17 @@ var app = express();
 app.set('view engine', 'ejs')
 
 app.use("/", express.static('public'));
-
 app.use(bodyParser.json());                        // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodies
 
-
-var con = mysql.createConnection({
+var db = {
   host: "195.208.51.37",
   user: "auth",
   password: "rai4quaiZu6U",
   database: "radius"
-});
+};
+var con = mysql.createConnection(db);
+
 
 function formatTel(tel) {
   return '+7 (' + tel.substring(0,3) + ') ' + tel.substring(3,6) + '-' + tel.substring(6,8) + '-' + tel.substring(8,10);
@@ -71,19 +71,45 @@ function isAuthorized(mac, callback) {
   });
 }
 
+var page = 'demo';
+var destination = 'http://www.wificomiten.ru';
 var demo_options = {
   'identity': 'restaurant',
   'loginUrl': 'http://localhost',
   'hostname': 'localhost',
   'username': '11:22:33:444:55:66',
   'password': '11:22:33:444:55:66',
-  'dst': 'http://metmos.ru',
+  'dst': destination,
   'tel': formatTel('1234567890'),
   'mac': '11:22:33:444:55:66',
 };
 
+
+function init(identity, server_name) {
+  var qd = 'SELECT start_page, page FROM devices WHERE identity = "' + identity + '" AND server_name = "' + server_name + '"';
+  log.verbose(qd);
+
+  mysql.createConnection(db).query(qd, function (err, rows) {
+    if (err) {
+      log.error(err);
+    // throw err;
+    return;
+    }
+
+    log.info(rows[0]);
+
+    if (rows[0].start_page != null) { 
+      page = rows[0].start_page; 
+    }
+    if (rows[0].page != null) { 
+      destination = rows[0].page; 
+    }
+  });
+}
+
 app.get('/', function (req, res) {
-  res.render('metropol', demo_options); 
+  log.info("Hello World!");
+  res.render('demo', demo_options); 
 });
 
 app.get('/tel', function(req, res) {
@@ -103,6 +129,8 @@ app.post('/', function (req, res) {
       '\nHotspot: ' + req.body['server-name'] +
       '\nMAC: ' + req.body.mac);
 
+  init(req.body.identity, req.body['server-name']);
+
   // Check if mac authorized
   isAuthorized(req.body.mac, function(authorized) {
     if (authorized) {
@@ -112,7 +140,7 @@ app.post('/', function (req, res) {
         'loginUrl': loginUrl,
         'username': req.body.mac,
         'password': req.body.mac,
-        'dst': 'http://metmos.ru',
+        'dst': destination,
       });
     }
     else {
@@ -143,14 +171,15 @@ app.post('/', function (req, res) {
           }
 
           var loginUrl = 'http://' + req.body.hostname + '/login';
-          log.info('Waiting for a call: ' + formatTel(tel) + ' from MAC ' + req.body.mac);
-          res.render('index.ejs', {
+          log.info('Waiting for a call from: ' + req.body.mac + ' to: ' + formatTel(tel));
+          
+          res.render(page, {
             'loginUrl': loginUrl,
             'hostname': req.body.hostname,
             'username': req.body.mac,
             'password': req.body.mac,
             'mac': req.body.mac,
-            'dst': 'http://metmos.ru',
+            'dst': destination,
             'tel': formatTel(tel),
           });
         });
